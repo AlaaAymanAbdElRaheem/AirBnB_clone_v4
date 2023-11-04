d6e2983c12a4f57dcf336dd5278eb7d321319722
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-11-04 17:17:15 +0200

    div#api_status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -16,5 +16,16 @@ $(document).ready(function(){
       $('#amenities_h4').text(Object.values(selected_amenity).join(', '));
     }
 
+    $.get("http://localhost:5001/api/v1/status/", function(data){
+      console.log(data);
+      if(data.status == 'OK'){
+        $('#api_status').addClass('available');
+      }else{
+        $('#api_status').removeClass('available');
+      }
+    }).fail(function() {
+      // Handle error here
+      console.error('Error occurred while fetching API status.');
+  });
   });
 });
