fc04d1fc4cc42c4830268af8072ba31df6c3f6c6
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-11-04 13:44:10 +0200

    fixing 1-hbnb.html

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,16 +4,17 @@ $(document).ready(function(){
   selected_amenity={}
 
   $('.amenity-checkbox').change(function(){
-    let id = $(this).attr('data-id')
-    let name = $(this).attr('data-name')
+    let id = $(this).attr('data-id');
+    let name = $(this).attr('data-name');
+    console.log('ID:', id, 'Name:', name);
     if ($(this).prop('checked')){
-      selected_amenity[id] = name
+      selected_amenity[id] = name;
     }else{
-      delete selected_amenity[id]
+      delete selected_amenity[id];
     }
 
     if(Object.keys(selected_amenity).length > 0){
-      $('#amenities_h4').text(Object.values(selected_amenity).join(', '))
+      $('#amenities_h4').text(Object.values(selected_amenity).join(', '));
     }
 
   });
