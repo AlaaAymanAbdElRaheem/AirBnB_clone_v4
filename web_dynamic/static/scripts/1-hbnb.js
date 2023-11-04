0da7895c3b012255d2f03b7d6831df919baab93a
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-11-04 12:04:41 +0200

    /1-hbnb

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+$(document).ready(function(){
+  selected_amenity={}
+
+  $('.amenity-checkbox').change(function(){
+    let id = $(this).attr('data-id')
+    let name = $(this).attr('data-name')
+    if ($(this).prop('checked')){
+      selected_amenity[id] = name
+    }else{
+      delete selected_amenity[id]
+    }
+
+    if(Object.keys(selected_amenity).length > 0){
+      $('#amenities_h4').text(Object.values(selected_amenity).join(', '))
+    }
+
+  });
+});
